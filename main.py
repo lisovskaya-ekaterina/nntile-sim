323945cb81dcd46957d1e5f2a145957919170a81
lisovskaya-ekaterina <149399679+lisovskaya-ekaterina@users.noreply.github.com>
2024-09-16 21:21:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from src.Worker import Worker
+from src.Scheduler import Scheduler
+from src.CPU import CPU
+from src.const import *
+import time 
+from src.tasks_generator import generate_task
+
+''' 
+Below: need to select eviction mode and pop task mode
+
+EVICTION_LRU the standard eviction policy in StarPU
+POP_TASK_DMDASD the pop policy of dmdasd scheduler
+'''
+
+eviction_mode = EVICTION_LRU
+# pop_task_mode = POP_TASK_DMDASD
+# eviction_mode = EVICTION_NEW_V1
+pop_task_mode = POP_TASK_NEW_V1
+
+
+
+
+task_list = generate_task()
+
+cpu = CPU()
+workers = [Worker(name=0, 
+                  memory_size=GPU_MEMORY_SIZE,
+                  memory=[], 
+                  cpu=cpu, 
+                  eviction_mode=eviction_mode,
+                  pop_task_mode=pop_task_mode)]
+scheduler = Scheduler()
+
+scheduler.do_work(task_list, workers)
+
+max_s = len(workers[0].queue)
+
+for worker in workers:
+    while True:
+        worker.pop_task()
+        print(f'{max_s-len(worker.queue)} of {max_s} ')
+        if len(worker.queue) == 0:
+            break
+    print(f'{worker.name} work time : {worker.work_time}')
+    print(f'{worker.name} n_load : {worker.n_load}')
