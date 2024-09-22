871cb5913c503058bd80a3c0f96125c24647b34c
kate <kate@bk.com>
2024-09-22 11:47:52 +0300

    upd

--- src/tasks_generator.py
+++ src/tasks_generator.py
@@ -11,29 +11,29 @@ def generate_task():
         lines = f.read()
     
     lines = lines.split('MPIRank: -1')
-    #lines.remove('')
+    # lines.remove('')
     for note in lines: 
         note = note.split("\n")
         note = remove_spaces_from_list(note)
         dictinory = dict(subString.split(": ") for subString in note)
-        print(dictinory)
         
-        if 'Sizes' not in dictinory.keys() or 'StartTime' not in dictinory.keys():
-            task_dict[dictinory['JobId']] = Task(id = dictinory['JobId'],
-                                                name = 'NAME',
-                                                task_duration = 0,
-                                                depends_on = [],
-                                                size = 67108864)
-            task_dict[dictinory['JobId']].status = STATUS_DONE
-        else: 
-            mode_letters_list = dictinory['Modes'].split()
-            for i in mode_letters_list:
-                if 'W' in i:
-                    index_of_w = mode_letters_list.index(i)
-            size = dictinory['Sizes'].split()
-            task_dict[dictinory['JobId']] = Task(id = dictinory['JobId'],
-                                                name = 'NAME',
-                                                task_duration=(float(dictinory['EndTime'])-float(dictinory['StartTime'])) / 1000,
-                                                depends_on=dictinory['DependsOn'].split(' ') if 'DependsOn' in dictinory.keys() else [],
-                                                size=int(size[index_of_w]))
+        if 'JobId' in dictinory.keys():
+            if 'Sizes' not in dictinory.keys() or 'StartTime' not in dictinory.keys():
+                task_dict[dictinory['JobId']] = Task(id = dictinory['JobId'],
+                                                    name = 'NAME',
+                                                    task_duration = 0,
+                                                    depends_on = [],
+                                                    size = 67108864)
+                task_dict[dictinory['JobId']].status = STATUS_DONE
+            else: 
+                mode_letters_list = dictinory['Modes'].split()
+                for i in mode_letters_list:
+                    if 'W' in i:
+                        index_of_w = mode_letters_list.index(i)
+                size = dictinory['Sizes'].split()
+                task_dict[dictinory['JobId']] = Task(id = dictinory['JobId'],
+                                                    name = 'NAME',
+                                                    task_duration=(float(dictinory['EndTime'])-float(dictinory['StartTime'])) / 1000,
+                                                    depends_on=dictinory['DependsOn'].split(' ') if 'DependsOn' in dictinory.keys() else [],
+                                                    size=int(size[index_of_w]))
     return task_dict
