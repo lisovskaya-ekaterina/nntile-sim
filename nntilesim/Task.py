235dc4cfd729212ce53fd1e3036d77cc72322d5e
rrenatovich <rsalimzyanov@yahoo.com>
2025-03-04 22:59:38 +0700

    1

--- nntilesim/Task.py
+++ nntilesim/Task.py
@@ -1,12 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 from .const import *
 
 @dataclass
 class Task:
+    """
+    Represents a task with its attributes.
+    """
     id: int
-    name : str
-    task_duration : float
-    size : int
-    depends_on : list
-    status : str = STATUS_INIT
-    unused_time : int = 0
+    name: str
+    task_duration: float
+    size: int
+    depends_on: List['Task'] = field(default_factory=list)
+    status: str = STATUS_INIT
+    unused_time: int = 0
