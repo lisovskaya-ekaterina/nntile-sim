1a3257c39a0399c0151edff900bad1b67f6bb2e5
rrenatovich <rsalimzyanov@yahoo.com>
2025-03-04 22:53:26 +0700

    Refactor Worker class for improved type hinting and code clarity

--- nntilesim/Worker.py
+++ nntilesim/Worker.py
@@ -1,18 +1,19 @@
 from .Task import Task
 from .CPU import CPU
 from .const import *
-#import random
 import sys
+from typing import List
+
 sys.setrecursionlimit(3000000)
 
-class WorkerMemory: 
-    def __init__(self, memory_size :float, name: int,memory: list[Task]):
+class WorkerMemory:
+    def __init__(self, memory_size: float, name: int, memory: List[Task]):
         self.memory_size = memory_size
         self.name = name
         self.memory = memory
-        
+
 class Worker:
-    def __init__(self, name : int, memory_size : float, memory: list[Task], cpu : CPU, eviction_mode : str, pop_task_mode : str):
+    def __init__(self, name: int, memory_size: float, memory: List[Task], cpu: CPU, eviction_mode: str, pop_task_mode: str):
         self.name = name
         self.current_task = None
         self.work_time = 0
@@ -24,44 +25,42 @@ class Worker:
         self.pop_task_mode = pop_task_mode
         self.memory = WorkerMemory(memory_size, name, memory)
 
-    def eviction(self):
+    def eviction(self) -> None:
         if self.eviction_mode == EVICTION_LRU:
             return self.eviction_LRU()
         elif self.eviction_mode == EVICTION_NEW_V1:
             return self.eviction_new_v1()
-    
-    def pop_task(self, workers):
+
+    def pop_task(self, workers: List['Worker']) -> None:
         if self.pop_task_mode == POP_TASK_DMDASD:
             return self.pop_task_dmdasd(workers)
         elif self.pop_task_mode == POP_TASK_NEW_V1:
             return self.pop_task_new_v1(workers)
-    
-    def eviction_LRU(self):
+
+    def eviction_LRU(self) -> None:
         '''
         Least Recently Used eviction policy
         By default in StarPU scheduling policies
-        Evictees data from worker memory that has not been used for the longest time
+        Evicts data from worker memory that has not been used for the longest time
         '''
         data = self.memory.memory[0]
         self.memory.memory.remove(data)
         self.cpu.memory.append(data)
-            
-    def pop_task_dmdasd(self, workers):
-        '''
-        Select the first task from the queue for which all the depends on tasks is done
-        '''
 
+    def pop_task_dmdasd(self, workers: List['Worker']) -> None:
+        '''
+        Select the first task from the queue for which all the depends on tasks are done
+        '''
         if self.current_task:
             self.queue.remove(self.current_task)
             self.current_task.status = STATUS_DONE
             self.memory.memory.append(self.current_task)
             self.work_time += self.current_task.task_duration
 
-        def can_execute(task):
+        def can_execute(task: Task) -> bool:
             return all(data.status == STATUS_DONE for data in task.depends_on)
-        
-        self.current_task = next((task for task in self.queue 
-                                  if task.status == STATUS_READY or can_execute(task)), None)
+
+        self.current_task = next((task for task in self.queue if task.status == STATUS_READY or can_execute(task)), None)
 
         if not self.current_task:
             return
@@ -69,20 +68,20 @@ class Worker:
         for d in self.current_task.depends_on:
             if d not in self.memory.memory:
                 self.load_data(d, workers)
-        
-        self.update_usless_data(self.current_task.depends_on)
-    
-    def eviction_new_v1(self):
+
+        self.update_useless_data(self.current_task.depends_on)
+
+    def eviction_new_v1(self) -> None:
         '''
-        A place to develop a new policy of eviction from the worker's memory. 
+        A place to develop a new policy of eviction from the worker's memory.
         When there is not enough space to load any new data.
         '''
         pass
 
-    def pop_task_new_v1(self, workers):
+    def pop_task_new_v1(self, workers: List['Worker']) -> None:
         '''
-        Select the first task for which all depends on tasks are already in the worker's memory. 
-        If there is no such task, then select the first task from the queue for which all the depends on tasks is done
+        Select the first task for which all depends on tasks are already in the worker's memory.
+        If there is no such task, then select the first task from the queue for which all the depends on tasks are done
         '''
         if self.current_task:
             self.queue.remove(self.current_task)
@@ -91,18 +90,17 @@ class Worker:
             self.work_time += self.current_task.task_duration
 
         done_data = set(task.id for task in self.memory.memory)
-        
-        def can_execute_in_mem(task):
+
+        def can_execute_in_mem(task: Task) -> bool:
             return all(data.status == STATUS_DONE and data.id in done_data for data in task.depends_on)
 
         self.current_task = next((task for task in self.queue if can_execute_in_mem(task)), None)
 
-        def can_execute(task):
+        def can_execute(task: Task) -> bool:
             return all(data.status == STATUS_DONE for data in task.depends_on)
-        
+
         if not self.current_task:
-            self.current_task = next((task for task in self.queue 
-                                    if task.status == STATUS_READY or can_execute(task)), None)
+            self.current_task = next((task for task in self.queue if task.status == STATUS_READY or can_execute(task)), None)
 
         if not self.current_task:
             return
@@ -110,19 +108,17 @@ class Worker:
         for d in self.current_task.depends_on:
             if d not in self.memory.memory:
                 self.load_data(d, workers)
-        
-        self.update_usless_data(self.current_task.depends_on)
 
-    def check_busy_space(self):
+        self.update_useless_data(self.current_task.depends_on)
+
+    def check_busy_space(self) -> float:
         '''
         A function that returns the current amount of occupied memory on the worker.
         '''
-        self.busy_space = 0
-        for elem in self.memory.memory:
-            self.busy_space += elem.size
+        self.busy_space = sum(elem.size for elem in self.memory.memory)
         return self.busy_space
-    
-    def update_usless_data(self, data_need_to_work : list [Task]):
+
+    def update_useless_data(self, data_need_to_work: List[Task]) -> None:
         '''
         A function that, at the task's start time, updates the least recently used counter for each piece of data in memory.
         '''
@@ -131,13 +127,13 @@ class Worker:
                 task.unused_time += 1
             else:
                 task.unused_time = 0
-        self.memory.memory = sorted(self.memory.memory, key = lambda x: x.unused_time, reverse = True)
+        self.memory.memory = sorted(self.memory.memory, key=lambda x: x.unused_time, reverse=True)
 
-    def load_data(self, data, workers):
+    def load_data(self, data: Task, workers: List['Worker']) -> None:
         '''
-        A function that has as input a data that needs to be loaded into the worker's memory. 
-        First, it check whether there is an opportunity to download, and, 
-        if necessary, evictees (according to the eviction policy) data from the worker's memory 
+        A function that has as input a data that needs to be loaded into the worker's memory.
+        First, it checks whether there is an opportunity to download, and,
+        if necessary, evicts (according to the eviction policy) data from the worker's memory
         until there is enough space for new data.
         After that, it loads the data into the worker's memory and deletes the data from the CPU's memory.
         The variables for collecting statistics (runtime and number of loads) are increased accordingly.
@@ -147,9 +143,9 @@ class Worker:
         self.memory.memory.append(data)
         if data in self.cpu.memory:
             self.cpu.memory.remove(data)
-        else: 
+        else:
             for w in workers:
-                if w.name != self.name and data in w.memory.memory: 
+                if w.name != self.name and data in w.memory.memory:
                     w.memory.memory.remove(data)
         self.work_time += data.size / TIME_DELIVERY_DATA
         self.n_load += 1
